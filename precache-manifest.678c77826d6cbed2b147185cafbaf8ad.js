a1612e5c2dbf356e5d6747939a4bd64582873cb3
cheesehao <anha.tra@gmail.com>
2022-04-20 11:55:04 +0700

    Updates

---
+++ precache-manifest.678c77826d6cbed2b147185cafbaf8ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aab1067e6838b9a336898d8c6f28272",
+    "url": "/hao/index.html"
+  },
+  {
+    "revision": "400ee30f9c01c8b48c5f",
+    "url": "/hao/static/css/2.fbf08e0d.chunk.css"
+  },
+  {
+    "revision": "718ada18ffa410fcc4c1",
+    "url": "/hao/static/css/main.e136cc0c.chunk.css"
+  },
+  {
+    "revision": "400ee30f9c01c8b48c5f",
+    "url": "/hao/static/js/2.5a73aaa0.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/hao/static/js/2.5a73aaa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "718ada18ffa410fcc4c1",
+    "url": "/hao/static/js/main.26d667d6.chunk.js"
+  },
+  {
+    "revision": "42424ecadd6c7f62fa94",
+    "url": "/hao/static/js/runtime-main.c44d2d76.js"
+  },
+  {
+    "revision": "c874fabbf6af6023c11bc105ec5d6ffd",
+    "url": "/hao/static/media/me.c874fabb.jpg"
+  }
+]);
